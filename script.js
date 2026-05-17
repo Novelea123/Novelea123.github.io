0886e1fafc7919f7d22dc2ddf6dc940d8f5904b8
Novelea123 <jewerly.by.jasmijn@gmail.com>
2026-05-17 19:07:06 +0200

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+let cart = [];
+let total = 0;
 
+function addToCart(product, price){
+
+  cart.push({product, price});
+
+  total += price;
+
+  updateCart();
+}
+
+function updateCart(){
+
+  const cartItems = document.getElementById("cart-items");
+
+  const totalElement = document.getElementById("total");
+
+  cartItems.innerHTML = "";
+
+  cart.forEach(item => {
+
+    const div = document.createElement("div");
+
+    div.classList.add("cart-item");
+
+    div.innerText = `${item.product} - €${item.price}`;
+
+    cartItems.appendChild(div);
+
+  });
+
+  totalElement.innerText = total.toFixed(2);
+
+}
